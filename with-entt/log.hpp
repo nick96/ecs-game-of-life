4f47f3ddf29e9d7ed4ae4b6a8c3003c233832768
Nick Spain <nicholas.spain96@gmail.com>
2020-01-12 16:37:12 +1100

    Allow turning of logging

--- with-entt/log.hpp
+++ with-entt/log.hpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#ifdef GOL_NO_LOG
+#define LOG(msg) do{}while(0)
+#else
 #define LOG(msg)                                                               \
     do {                                                                       \
         std::cerr << __FILE__ << ":" << __LINE__ << ": " << msg << std::endl;  \
     } while (0)
+#endif
